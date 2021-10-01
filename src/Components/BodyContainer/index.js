fba3c36ea3bda28613d1d840944230fcc01dd0ed
Eno Ekanem <e.ekanemeno@gmail.com>
2021-10-01 04:52:30 +0100

    hide API key

--- src/Components/BodyContainer/index.js
+++ src/Components/BodyContainer/index.js
@@ -1,159 +1,160 @@
-import React, { useState } from 'react';
-import { BarLoader, PropagateLoader } from 'react-spinners';
+import React, { useState } from "react";
+import { BarLoader, PropagateLoader } from "react-spinners";
 
 import {
-  MainContainerTopItems,
-  MainContainerTop,
-  TitleH1,
-  SearchInput,
-  MainContainerBottom,
-  SearchContainer,
-  SearchIcon,
-  Container,
-  MainContainerBottomItems,
-  DataContainer,
-  BottomIcon,
-} from './BodyContainerItems';
+	MainContainerTopItems,
+	MainContainerTop,
+	TitleH1,
+	SearchInput,
+	MainContainerBottom,
+	SearchContainer,
+	SearchIcon,
+	Container,
+	MainContainerBottomItems,
+	DataContainer,
+	BottomIcon,
+} from "./BodyContainerItems";
 
-import { DataEntry } from '../DataEntry/DataEntry';
-import Map from '../Map';
+import { DataEntry } from "../DataEntry/DataEntry";
+import { config } from "../../config/config";
+import Map from "../Map";
 
 const BodyContainer = () => {
-  const [searchInput, setSearchInput] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [isError, setIsError] = useState(null);
-  const [userIpData, setUserIpData] = useState(null);
+	const [searchInput, setSearchInput] = useState("");
+	const [loading, setLoading] = useState(false);
+	const [isError, setIsError] = useState(null);
+	const [userIpData, setUserIpData] = useState(null);
 
-  // API DATA CALL HANDLER
+	// API DATA CALL HANDLER
 
-  const getData = async () => {
-    if (searchInput === '') {
-      alert('Please Enter IP Address');
-      return;
-    }
+	const getData = async () => {
+		if (searchInput === "") {
+			alert("Please Enter IP Address");
+			return;
+		}
 
-    setLoading(true);
-    try {
-      const URL_PATH = `
-      https://geo.ipify.org/api/v1?apiKey=at_n7KZsfggQnIAEce406OJbCzW488XO&ipAddress=${searchInput}&domain=${searchInput}`;
-      const api_call = await fetch(URL_PATH);
-      const response = await api_call.json();
+		setLoading(true);
+		try {
+			const URL_PATH = `
+      https://geo.ipify.org/api/v1?apiKey=${config.ipAddressKey}&ipAddress=${searchInput}&domain=${searchInput}`;
+			const api_call = await fetch(URL_PATH);
+			const response = await api_call.json();
 
-      console.log(response);
+			console.log(response);
 
-      if (
-        !response ||
-        response.length === 0 ||
-        response.code === 422 ||
-        response.code === 400
-      ) {
-        setLoading(false);
-        setIsError(response.messages);
-        setUserIpData(null);
-        return;
-      }
+			if (
+				!response ||
+				response.length === 0 ||
+				response.code === 422 ||
+				response.code === 400
+			) {
+				setLoading(false);
+				setIsError(response.messages);
+				setUserIpData(null);
+				return;
+			}
 
-      setUserIpData(response);
+			setUserIpData(response);
 
-      setLoading(false);
-      setIsError('');
-    } catch (err) {
-      console.log(err);
-    }
-  };
+			setLoading(false);
+			setIsError("");
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-  const override = `
+	const override = `
   display: block;
   margin: 0 auto;
   border-color: red;
   
   `;
 
-  // const position = [51.505, -0.09];
+	// const position = [51.505, -0.09];
 
-  return (
-    <>
-      <Container>
-        <MainContainerTop>
-          <MainContainerTopItems>
-            <TitleH1>IP Address Tracker</TitleH1>
-            <SearchContainer>
-              <SearchInput
-                placeholder={'Search for any IP address or domain'}
-                onChange={(e) => setSearchInput(e.target.value)}
-                onKeyPress={(e) => (e.key === 'Enter' ? getData() : null)}
-              />
-              <SearchIcon onClick={() => getData()} />
-            </SearchContainer>
+	return (
+		<>
+			<Container>
+				<MainContainerTop>
+					<MainContainerTopItems>
+						<TitleH1>IP Address Tracker</TitleH1>
+						<SearchContainer>
+							<SearchInput
+								placeholder={"Search for any IP address or domain"}
+								onChange={(e) => setSearchInput(e.target.value)}
+								onKeyPress={(e) => (e.key === "Enter" ? getData() : null)}
+							/>
+							<SearchIcon onClick={() => getData()} />
+						</SearchContainer>
 
-            {isError ? <DataContainer>{isError}</DataContainer> : ''}
+						{isError ? <DataContainer>{isError}</DataContainer> : ""}
 
-            {loading ? (
-              <DataContainer>
-                <BarLoader
-                  css={override}
-                  size={200}
-                  color={'black'}
-                  loading={loading}
-                />
-              </DataContainer>
-            ) : (
-              <>
-                {userIpData && (
-                  <>
-                    <div>
-                      <DataContainer>
-                        <DataEntry title={'Ip Address'} data={userIpData.ip} />
-                        <DataEntry
-                          title={'Location'}
-                          data={`${userIpData.location.city} , 
+						{loading ? (
+							<DataContainer>
+								<BarLoader
+									css={override}
+									size={200}
+									color={"black"}
+									loading={loading}
+								/>
+							</DataContainer>
+						) : (
+							<>
+								{userIpData && (
+									<>
+										<div>
+											<DataContainer>
+												<DataEntry title={"Ip Address"} data={userIpData.ip} />
+												<DataEntry
+													title={"Location"}
+													data={`${userIpData.location.city} , 
                       ${userIpData.location.region}, 
                       ${userIpData.location.postalCode}`}
-                        />
-                        <DataEntry
-                          title={'Timezone'}
-                          data={`UTC ${userIpData.location.timezone}`}
-                        />
-                        <DataEntry title={'Isp'} data={userIpData.isp} />
-                      </DataContainer>
-                    </div>
-                  </>
-                )}
-              </>
-            )}
-          </MainContainerTopItems>
-        </MainContainerTop>
+												/>
+												<DataEntry
+													title={"Timezone"}
+													data={`UTC ${userIpData.location.timezone}`}
+												/>
+												<DataEntry title={"Isp"} data={userIpData.isp} />
+											</DataContainer>
+										</div>
+									</>
+								)}
+							</>
+						)}
+					</MainContainerTopItems>
+				</MainContainerTop>
 
-        {loading ? (
-          <MainContainerBottom>
-            <PropagateLoader
-              css={override}
-              size={15}
-              color={'black'}
-              loading={loading}
-            />
-          </MainContainerBottom>
-        ) : (
-          <>
-            {userIpData ? (
-              <>
-                <MainContainerBottom>
-                  <Map
-                    lat={userIpData.location.lat}
-                    lng={userIpData.location.lng}
-                  />
-                </MainContainerBottom>
-              </>
-            ) : (
-              <MainContainerBottom>
-                <MainContainerBottomItems></MainContainerBottomItems>
-              </MainContainerBottom>
-            )}
-          </>
-        )}
-      </Container>
-    </>
-  );
+				{loading ? (
+					<MainContainerBottom>
+						<PropagateLoader
+							css={override}
+							size={15}
+							color={"black"}
+							loading={loading}
+						/>
+					</MainContainerBottom>
+				) : (
+					<>
+						{userIpData ? (
+							<>
+								<MainContainerBottom>
+									<Map
+										lat={userIpData.location.lat}
+										lng={userIpData.location.lng}
+									/>
+								</MainContainerBottom>
+							</>
+						) : (
+							<MainContainerBottom>
+								<MainContainerBottomItems></MainContainerBottomItems>
+							</MainContainerBottom>
+						)}
+					</>
+				)}
+			</Container>
+		</>
+	);
 };
 
 export default BodyContainer;
---
+++ src/config/config.js
@@ -0,0 +1,3 @@
+export const config = {
+	ipAddressKey: process.env.REACT_APP_GEO_KEY,
+};
